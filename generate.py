26dd08b4bc94b3abaf0326ecbf6ca098491ce9e2
xarvatium <ctfore2003@gmail.com>
2021-02-14 17:07:17 -0500

    Random YouTube function is now live! Also included: random color, a new ask feature, and an improved help menu (that's actually helpful)

--- generate.py
+++ generate.py
@@ -1,18 +1,36 @@
 import wikipedia
 import discord
-import youtube_dl
 from random import *
 from discord.ext import commands
+from googleapiclient.discovery import build
+from PIL import Image
+from keys import *
+import os
+import requests
 
-client = commands.Bot(command_prefix=';', help_command=None)
+bot = commands.Bot(command_prefix=';', help_command=None)
+
+# Gives the even that tells the bot is up, also sets the status
+@bot.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(bot))
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.watching,
+            name="In Maintenance | ;"
+        )
+    )
 
 
-@client.group()
+# Sets the "generate" group for the ;generate <command> group
+@bot.group()
 async def generate(ctx):
     if ctx.invoked_subcommand is None:
-        await ctx.channel.send('Error: must include what to randomly generate.')
+        nullEmbed = discord.Embed(title="Error", description="Please include what to generate randomly. If you don't know what this is, use the ;help command!", color=0xB87DDF)
+        await ctx.channel.send(embed=nullEmbed)
 
 
+# Generates a random Wikipedia article
 @generate.command()
 async def article(ctx):
     wiki_page = wikipedia.random(1)
@@ -28,6 +46,7 @@ async def article(ctx):
         await ctx.channel.send("The article you requested had a disambiguation, please try again.")
 
 
+# Generates a random number between the <low> and the <high> given by the user
 @generate.command()
 async def number(ctx, low: int, high: int):
     numError = "There was an error! Did you make sure you included a minimum/maximum or to give numbers and not words?"
@@ -42,31 +61,73 @@ async def number(ctx, low: int, high: int):
         await ctx.channel.send(embed=errorEmbed)
 
 
+# Generates a random YouTube video
 @generate.command()
 async def video(ctx):
     import random
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
 
-    characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-                  "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-                  "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d",
-                  "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                  "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
-                  "y", "z", "-", "_"]
-    result = ""
-    for i in range(0, 11):
-        result += random.choice(characters)
-    print(result)
-    with ydl:
-        result2 = ydl.extract_info(
-            'http://youtube.com/watch?v=' + result,
-            download=False
-        )
-    if 'entries' in result2:
-        videoOut = result2['entries'][0]
-    else:
-        videoOut = result2
-    print(videoOut)
-    video_url = video['url']
-    await ctx.channel.send(video_url)
+    # Defines the variables to be used
+    youtubeApiServiceName = 'youtube'
+    youtubeApiVersion = "v3"
+    prefix = ['IMG ', 'IMG_', 'IMG-', 'DSC ']
+    postfix = [' MOV', '.MOV', ' .MOV']
+
+    # This is what it uses to search
+    def youtube_search():
+
+        # Giving my API Key and Developer Key to Google's API
+        youtube = build(youtubeApiServiceName, youtubeApiVersion, developerKey=DEVELOPER_KEY)
+
+        # This is the actual code that searches YouTube and gives 5 results back, and chooses one
+        searchResponse = youtube.search().list(
+            q=random.choice(prefix) + str(random.randint(999, 9999)) + random.choice(postfix),
+            part='snippet',
+            maxResults=5
+        ).execute()
+
+        videos = []
+
+        # This is the For statement that looks for actual YouTube videos and not just strings that fit Google's naming scheme
+        for search_result in searchResponse.get('items', []):
+            if search_result['id']['kind'] == 'youtube#video':
+                videos.append('%s' % (search_result['id']['videoId']))
+        return videos[random.randint(0, 2)]
+
+    # The portion that is sent to Discord, with the base URL preceding the search results
+    await ctx.channel.send("https://youtube.com/watch?v="+youtube_search())
+    print("Sent YT video")
+
+
+# Gives a random color sent in an embed and a file
+@generate.command()
+async def color(ctx):
+    # Defines the RGB to Hex function
+    def rgb_to_hex(rgb):
+        r,g,b = rgb
+        return '#%02x%02x%02x' % (r,g,b)
+
+    # This sets up the variables to be used
+    colorRGB = (randint(0,255), randint(0,255), randint(0,255))
+    colorHex = rgb_to_hex(colorRGB)
+    colorDesc = "Hex Code: " + "**" + colorHex + "**" + "\n" + "RGB Code: " + "**" + str(colorRGB) + "**"
+    width = 128
+    height = 128
+
+    # Creates an image and saves it then puts it into a Discord embed
+    img = Image.new(
+        mode="RGB",
+        size=(width, height),
+        color=colorRGB
+    )
+    img.save("color.png")
+    file = discord.File("color.png")
+    colorEmbed = discord.Embed(
+        title="Your Color is:",
+        description=colorDesc,
+        color=0xB87DDF
+    )
+    # What is actually sent to Discord
+    colorEmbed.set_image(url="attachment://color.png")
+    await ctx.channel.send(file=file, embed=colorEmbed)
+    print("Sent image")
+    os.remove("color.png")
--- main.py
+++ main.py
@@ -1,46 +1,28 @@
 import discord
-import tracemalloc
 import os
 import asyncio
-import random
-import youtube_dl
 from discord.ext import commands
 from dotenv import load_dotenv
-from random import choice
 
-
-client = commands.Bot(command_prefix=';', help_command=None)
-tracemalloc.start()
-numError = "There was an error! Did you make sure you included a minimum/maximum or to give numbers and not words?"
-
-# Imports the generate file
 from generate import *
 
-
-@client.event
-async def on_ready():
-    print('Logged in as {0.user}'.format(client))
-    await client.change_presence(
-        activity=discord.Activity(
-            type=discord.ActivityType.watching,
-            name="In Maintenance | ;"
-        )
-    )
-
-
-@client.command()
+@bot.command()
 async def help(ctx):
-    page1embed = discord.Embed(title="Page 1",
-                               description="Placeholder"
-                               )
-    contents = ["This is page 1!", "This is page 2!", "This is page 3!", "This is page 4!"]
-    pages = 4
-    cur_page = 1
-    message = await ctx.send(f"Page {cur_page}/{pages}:\n{contents[cur_page - 1]}")
-    # getting the message object for editing and reacting
-
-    await message.add_reaction("◀️")
-    await message.add_reaction("▶️")
+    helpEmbed = discord.Embed(title="Help Page",
+                              description="__Bot Prefix is: **;**__",
+                              color=0xB87DDF
+                              )
+    helpEmbed.add_field(name="General Commands",
+                        value="\;ping - checks the bot's latency\n;repeat - repeats what you say (in beta)\n;ask - gives 8Ball answers",
+                        inline=False
+                        )
+    helpEmbed.add_field(name="Random Generator Commands",
+                        value=";generate article - gives a random wikipedia article\n;generate video - gives a random YouTube video\n;generate number [low] [high] - gives a random number in between the two given values\n;generate color - gives a random color + it's hex and RGB code",
+                        inline=False
+                        )
+    helpEmbed.set_footer(text="Creator: Xarvatium#6561", icon_url="https://cdn.discordapp.com/avatars/514866599400833034/88a61a2683879b72622d4f9990dc6d2b.png?size=128")
+    helpEmbed.set_author(name="The Random Bot", icon_url="https://cdn.discordapp.com/avatars/755986454907191319/5ff8f9b7b1eebb4650e8ee94e65c03d6.png?size=128")
+    message = await ctx.channel.send(embed=helpEmbed)
     await message.add_reaction("❌")
 
     def check(reaction, user):
@@ -49,69 +31,65 @@ async def help(ctx):
 
     while True:
         try:
-            reaction, user = await client.wait_for("reaction_add", timeout=60, check=check)
+            reaction, user = await bot.wait_for("reaction_add", timeout=180, check=check)
             # waiting for a reaction to be added - times out after x seconds, 60 in this
-            # example
-
-            if str(reaction.emoji) == "▶️" and cur_page != pages:
-                cur_page += 1
-                await message.edit(content=f"Page {cur_page}/{pages}:\n{contents[cur_page - 1]}")
-                await message.remove_reaction(reaction, user)
-
-            elif str(reaction.emoji) == "◀️" and cur_page > 1:
-                cur_page -= 1
-                await message.edit(content=f"Page {cur_page}/{pages}:\n{contents[cur_page - 1]}")
-                await message.remove_reaction(reaction, user)
-
-            elif str(reaction.emoji) == "❌":
+            if str(reaction.emoji) == "❌":
                 await message.delete()
-
-            else:
-                await message.remove_reaction(reaction, user)
-                # removes reactions if the user tries to go forward on the last page or
-                # backwards on the first page
         except asyncio.TimeoutError:
             await message.delete()
             break
             # ending the loop if user doesn't react after x seconds
 
 
-@client.command()
+@bot.command()
 async def status(ctx, *, content):
     import developers
     dev_list = developers.dev_list["Developers"]["User IDs"]
     if ctx.message.author.id in dev_list:
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=content))
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=content))
 
     else:
         await ctx.channel.send("Sorry! It appears you don't have permission to use this command.")
 
 
-@client.command()
+@bot.command()
 async def ping(ctx):
-    ping = client.latency * 1000
+    ping = bot.latency * 1000
     pingEmbed = discord.Embed(title="Pong! :ping_pong:", description=f"My latency is: {int(ping)}ms", color=0xB87DDF)
     await ctx.send(embed=pingEmbed)
 
 
-@client.command()
+@bot.command()
 async def monke(ctx):
     await ctx.send(
         "https://tenor.com/view/obese-monkey-fat-monkey-summer-belly-eating-lettuce-summer-look-gif-13014350"
     )
 
 
-@client.command()
+@bot.command()
 async def repeat(ctx, *, user_in: str):
-    if user_in == str.lower("i am stupid") or str.lower("i'm stupid") or str.lower("im stupid"):
+    if user_in.lower().startswith("im"):
         await ctx.send("yea we know")
-    elif user_in == str.lower("I am gay") or str.lower("I'm gay") or str.lower("Im gay"):
+    elif user_in.lower().startswith('i '):
         await ctx.send("yea we know")
+    elif user_in.lower().startswith("i'm"):
+        await ctx.send("yea we know")
+    elif user_in.lower() == "@everyone":
+        await ctx.send("no :)")
     else:
         await ctx.send(user_in)
 
+@bot.command()
+async def ask(ctx, *, content):
+    import random
+    responses = ["It is certain", "Without a doubt", "You may rely on it", "Yes, definitely", "It is decidedly so",
+            "As I see it, yes", "Most likely", "Yes", "Outlook good", "Signs point to yes", "Reply hazy try again",
+            "Better not tell you now", "Ask again later", "Cannot predict now", "Concentrate and ask again",
+            "Don't count on it", "Outlook not so good", "My sources say no", "Very doubtful", "My reply is no"]
+    answer = random.choice(responses)
+    await ctx.channel.send(answer)
 
 # The Token initialization
-with open('/home/pi/Desktop/token.txt','r') as token:
-    token1 = token.read()
-client.run(token1)
+load_dotenv()
+token = os.getenv('discord_token')
+bot.run(token)
