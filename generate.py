e4c3af0a89681584c1e3c733c9b0489e159deab0
xarvatium <ctfore2003@gmail.com>
2021-02-15 15:18:47 -0500

    Fixed the status.

--- generate.py
+++ generate.py
@@ -23,7 +23,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name="In Maintenance | ;"
+            name="Randomness | ;"
         )
     )
 
